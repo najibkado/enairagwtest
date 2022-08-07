948dc5d48503d592575b491cc4216972888181ef
najibkado <dopeland3@gmail.com>
2022-08-07 06:58:58 +0100

    Added allowed hosts

--- ShadeStore/settings.py
+++ ShadeStore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm81dt-xvi53um4jicg1t$pvudu6bmd)_w0+k1mnjlrn(4x7l2f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['46e357eed099.ngrok.io', '127.0.0.1']
+ALLOWED_HOSTS = ['enairagwtest.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
